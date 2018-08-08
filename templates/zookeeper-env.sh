a7a894b246f35589e6d37245b49ed7fc84d500ca
Stefan Horning <stefan@hornings.de>
2018-08-08 17:57:06 +0200

    Added more stubs for still missing parts. Finished handlers

---
+++ templates/zookeeper-env.sh
@@ -0,0 +1,5 @@
+# {{ ansible_managed }}
+# Env file for zookeeper startup.
+# Has to be named zookeeper-env.sh to be loaded by bin/zkEnv.sh (whish is in turn called by zkServer.sh).
+
+# TODO: add stuff
