3752c1bf09b0f84f9551e1bfccd357bfb251ea5b
Stefan Horning <stefan@hornings.de>
2018-08-09 12:49:34 +0200

    First feature complete, but completely untested version

--- templates/zookeeper-env.sh
+++ templates/zookeeper-env.sh
@@ -2,4 +2,8 @@
 # Env file for zookeeper startup.
 # Has to be named zookeeper-env.sh to be loaded by bin/zkEnv.sh (whish is in turn called by zkServer.sh).
 
-# TODO: add stuff
+{% for key, value in zookeeper_env_variables.items() | sort %}
+{{ key }}={{ value }}
+{% endfor %}
+
+SERVER_JVMFLAGS="-Xms{{ zookeeper_jvm_heap_size }} -Xmx{{ zookeeper_jvm_heap_size }}"
